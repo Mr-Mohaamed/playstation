d2302fd4f86937599d960e1c53fcb3362927f432
ahmedreo5 <website_dev@apexmindcoders.com>
2024-10-05 17:12:01 +0300

    Update SonyBar.jsx

--- src/components/SonyBar.jsx
+++ src/components/SonyBar.jsx
@@ -1,6 +1,6 @@
 const SonyBar = () => {
   return (
-    <div className="hidden mobile:block h-[36px] bg-black text-right pr-4 text-white font-extrabold flex items-center justify-end">
+    <div className="hidden mobile:flex h-[36px] bg-black text-right pr-4 text-white font-extrabold  items-center justify-end">
       SONY
     </div>
   );
